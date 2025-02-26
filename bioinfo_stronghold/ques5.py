e786349be9197c91cc525c71f2f4392f490ddff5
Arushi2798 <arushi1301@gmail.com>
2025-02-26 22:33:58 +0530

    code update

--- bioinfo_stronghold/ques5.py
+++ bioinfo_stronghold/ques5.py
@@ -8,3 +8,14 @@ Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
 
 Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below."""
 
+file =open("file1.txt","r")
+content=file.readlines()
+read=[]
+# print(content)
+for i in range(len(content)):
+    if content[i].startswith(">"):
+        continue
+    else:
+        read.append(content[i])
+
+print(read)
