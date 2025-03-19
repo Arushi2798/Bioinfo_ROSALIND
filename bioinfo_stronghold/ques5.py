94b90ddbc40fa19fcb044213ace2a2caf3e771d4
Arushi2798 <arushi1301@gmail.com>
2025-03-19 20:48:59 +0530

    code update

--- bioinfo_stronghold/ques5.py
+++ bioinfo_stronghold/ques5.py
@@ -8,28 +8,42 @@ Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
 
 Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below."""
 
-file =open("file1.txt","r")
+file =open("file2.txt","r")
 content=file.readlines()
 for each in content:
     content1=(" ".join(content)).split(">")
 
 # print(content1[0])
 content1.pop(0)
-# print(content1[0])
+# print(content1)
 # print(len(content1))
 
+d={}
 
 for i in range(len(content1)):
-    pass
-gc=0
-d={}
-for each in read:
-    for i in range(len(each)):
-        if each[i]=="G" or each[i]=="C":
-            gc+=1
-    gcp=(gc/len(each))*100
-    d[each]=gcp
+    gc,total_len=0,0
+    text=content1[i].split("\n ")
+    text.pop()
+    # print(text)
+    for j in range(1,len(text)):
+        # print(text[j])
+        for each in text[j]:
+            if each=="G" or each=="C":
+                gc+=1
+                total_len+=1
+            elif each=="A" or each== "T":
+                total_len+=1
 
+    gcp=(gc/total_len)*100
+    d[text[0]]=gcp
+    
 
 # print(d)
-# print(read)
+max_val=0
+# res=max(d.values())
+for key,val in d.items():
+    if val > max_val:
+        max_val= val
+        max_id = key
+print(id, max_val)
+
