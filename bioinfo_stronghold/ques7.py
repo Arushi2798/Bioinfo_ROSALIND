605ab965e696122bb8d168177f951ea797efc149
Arushi2798 <arushi1301@gmail.com>
2025-03-20 19:26:50 +0530

    code update

--- bioinfo_stronghold/ques7.py
+++ bioinfo_stronghold/ques7.py
@@ -3,3 +3,16 @@ organisms: k individuals are homozygous dominant for a factor, m are heterozygou
 are homozygous recessive.
 
 Return: The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate."""
+
+import random
+k,m,n=2,2,2
+p=k+m+n
+
+indiv=[]
+indiv.append(random.randint(0,p))
+indiv.append(random.randint(0,p))
+
+# print(indiv)
+#in case of homozygous dominant
+#in case of heterozygous
+#in case of homozygous recessive
