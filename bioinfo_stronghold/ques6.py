dcdc2d0f09b844022852fb1a92f79c1a9a17e6a0
Arushi2798 <arushi1301@gmail.com>
2025-03-20 19:06:49 +0530

    code update

---
+++ bioinfo_stronghold/ques6.py
@@ -0,0 +1,15 @@
+"""
+Given two strings s and t of equal length, the Hamming distance between s
+and t, denoted dH(s,t), is the number of corresponding symbols that differ in s
+and t.
+Given: Two DNA strings sand t of equal length (not exceeding 1 kbp).
+
+Return: The Hamming distance dH(s,t)"""
+s="GAGCCTACTAACGGGAT"
+t="CATCGTAATGACGGCCT"
+dh=0
+if len(s)==len(t):
+    for i in range(len(s)):
+        if s[i]!=t[i]:
+            dh+=1
+print(dh)
