89c597a343e29f65526fffc026194a50dd0443fa
Arushi2798 <arushi1301@gmail.com>
2025-12-26 14:24:46 +0530

    file updated

---
+++ python_village.py
@@ -0,0 +1,60 @@
+#q1:
+# import this
+
+# q2:
+a=int(input("a: "))
+b=int(input("b: "))
+hsquare=a**2+b**2
+print(hsquare)
+
+# a=int(input("a: "))
+# b=int(input("b: "))
+# sum=0
+# for i in range(a,b+1):
+#     if i%2!=0:
+#         sum+=i
+# print(sum)
+
+# f= open("C:/Users/arush/Downloads/rosalind_ini5.txt","r")
+# o=open("C:/Users/arush/Downloads/output.txt","w")
+# text1=f.readlines()[1::2]
+# for each in text1:
+#     each.splitlines()
+#     o.write(each)
+
+# f.close()
+# o.close()
+
+# d={}
+# s=input("give the string : \n")
+# res=s.split()
+# # print(res)
+# for i in range(len(res)):
+#     count=0
+#     for j in range(len(res)):
+#         if res[i] == res[j]:
+#             count+=1
+#     d[res[i]]=count
+# print(d)
+
+# o=open("C:/Users/arush/Downloads/output.txt","w")
+# for key, value in d.items():
+#     o.write(f"{key} {value}\n")
+# o.close()
+
+
+##
+#bioinfo
+# lenth=0
+# gc=0
+# s="CCACCCTCG"
+# for each in s:
+#     if each =="A" or each=="T":
+#         lenth+=1
+#     elif each== "G" or each=="C":
+#         lenth+=1
+#         gc+=1
+
+# print(f'gc: {gc} for len {lenth}')
+# print((gc/lenth)*100)
+
